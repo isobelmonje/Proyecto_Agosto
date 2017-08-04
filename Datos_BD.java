522b6754192a9f4d58d1cb171d559b64015e2e64
Plokoon1987 <plokoon1987@gmail.com>
2017-08-04 13:50:27 +0200

    Modificacion a Inicializacion de Encargados en Datos_BD

--- Datos_BD.java
+++ Datos_BD.java
@@ -41,21 +41,12 @@ public class Datos_BD
                                 int ID_encargado)
     {
         Encargado encargado = new Encargado();
-      
-        encargado.modificar_DNI(DNI);
-        encargado.modificar_direccion(direccion);
-        encargado.modificar_nombre(nombre);
-        encargado.modificar_primer_apellido(primer_apellido);
-        encargado.modificar_segundo_apellido(segundo_apellido);
-        encargado.modificar_fecha_de_nacimiento(fecha_nacimiento);
-        encargado.modificar_fecha_alta(fecha_alta);
-        encargado.modificar_turno(turno);
-           
-        encargado.modificar_tlfn_per(tlfn_personal);
-        encargado.modificar_ID_trabajador(ID_trabajador);
-        encargado.modificar_tlfn_empresa(tlfn_empresa);
-        encargado.modificar_ID_encargado(ID_encargado);
-
+        
+        encargado.modificar_ID_encargado(DNI, direccion, nombre, primer_apellido, 
+                                segundo_apellido, fecha_nacimiento, fecha_alta, 
+                                turno, tlfn_personal, tlfn_empresa, ID_trabajador, 
+                                ID_encargado);
+                                
         lista_encargados.add(encargado);
     }
     
@@ -88,113 +79,27 @@ public class Datos_BD
             encargado.resumen_encargado_completo();
         }
     }
-    
-    
-      
+              
     private void rellena_encargados_iniciales()
     {
-        int num_enc = 5;
-        int string_columnas = 8;
-        int integer_columnas = 4;
-        Encargado encargado;
-                
-        // STRINGS de encargados:
-        String[][] String_data_enc = new String[string_columnas][num_enc];
+        nuevo_encargado("11111111A", "Madrid", "Pablo", "Iglesias", 
+                        "Turrion", "18/01/1987", "01/01/2001", "mañana", 
+                        121232343, 662872195, 1, 101);
+
+        nuevo_encargado("22222222B", "Madrid", "Pedro", "Sanchez", 
+                        "Castejón", "01/01/1981", "02/02/2002", "tarde", 
+                        232343454, 662872195, 2, 202);
         
-        // DNI de encargados;
-        String_data_enc[0][0] = "11111111A";
-        String_data_enc[0][1] = "22222222B";
-        String_data_enc[0][2] = "33333333C";
-        String_data_enc[0][3] = "44444444D";
-        String_data_enc[0][4] = "55555555E";
+        nuevo_encargado("33333333C", "Galicia", "Mariano", "Rajoy", 
+                        "Brey", "03/03/1983", "03/03/2003", "mañana", 
+                        343454565, 662872195, 3, 303);
+                        
+        nuevo_encargado("44444444D", "Andalucia", "Alberto", "Garzón", 
+                        "Espinosa", "04/04/1984", "04/04/2004", "tarde", 
+                        454565676, 662872195, 4, 404);
         
-        // Direccion de encargados:
-        String_data_enc[1][0] = "Madrid";
-        String_data_enc[1][1] = "Madrid";
-        String_data_enc[1][2] = "Galicia";
-        String_data_enc[1][3] = "Andalucia";
-        String_data_enc[1][4] = "Cataluña";
-        
-        // Nombre de encargados:
-        String_data_enc[2][0] = "Pablo";
-        String_data_enc[2][1] = "Pedro";
-        String_data_enc[2][2] = "Mariano";
-        String_data_enc[2][3] = "Alberto";
-        String_data_enc[2][4] = "Albert";
-        
-        // Primer apellido de encargados:
-        String_data_enc[3][0] = "Iglesias";
-        String_data_enc[3][1] = "Sanchez";
-        String_data_enc[3][2] = "Rajoy";
-        String_data_enc[3][3] = "Garzon";
-        String_data_enc[3][4] = "Rivera";
-        
-        // Segundo Apellido de encargados:
-        String_data_enc[4][0] = "Turrion";
-        String_data_enc[4][1] = "Castejon";
-        String_data_enc[4][2] = "Brey";
-        String_data_enc[4][3] = "Espinosa";
-        String_data_enc[4][4] = "Diaz";
-        
-        // Fecha de nacimiento de encargados:
-        String_data_enc[5][0] = "01/01/1981";
-        String_data_enc[5][1] = "02/02/1982";
-        String_data_enc[5][2] = "03/03/1983";
-        String_data_enc[5][3] = "04/04/1984";
-        String_data_enc[5][4] = "05/05/1985";
-        
-        // Fecha de alta de encargados:
-        String_data_enc[6][0] = "01/01/2001";
-        String_data_enc[6][1] = "02/02/2002";
-        String_data_enc[6][2] = "03/03/2003";
-        String_data_enc[6][3] = "04/04/2004";
-        String_data_enc[6][4] = "05/05/2005";
-        
-        // Fecha de alta de encargados:
-        String_data_enc[7][0] = "mañana";
-        String_data_enc[7][1] = "tarde";
-        String_data_enc[7][2] = "mañana";
-        String_data_enc[7][3] = "tarde";
-        String_data_enc[7][4] = "fin de semana";
-        
-        
-        // INTEGERS de encargados:
-        int[][] int_data_enc = new int[integer_columnas][num_enc];
-                     
-        // Telefono personal de encargados:
-        int_data_enc[0][0] = 121232343;
-        int_data_enc[0][1] = 232343454;
-        int_data_enc[0][2] = 343454565;
-        int_data_enc[0][3] = 454565676;
-        int_data_enc[0][4] = 565676787;
-        
-        // ID_trabajador de encargados:
-        int_data_enc[1][0] = 1;
-        int_data_enc[1][1] = 2;
-        int_data_enc[1][2] = 3;
-        int_data_enc[1][3] = 4;
-        int_data_enc[1][4] = 5;
-        
-        // Telefono de trabajo de encargados:
-        int_data_enc[2][0] = 666555001;
-        int_data_enc[2][1] = 666555002;
-        int_data_enc[2][2] = 666555003;
-        int_data_enc[2][3] = 666555004;
-        int_data_enc[2][4] = 666555005;
-        
-        // ID_encargado de encargados:
-        int_data_enc[3][0] = 101;
-        int_data_enc[3][1] = 102;
-        int_data_enc[3][2] = 103;
-        int_data_enc[3][3] = 104;
-        int_data_enc[3][4] = 105;
-        
-        
-        for (int i = 0; i <num_enc; i++){
-            nuevo_encargado(String_data_enc[0][i], String_data_enc[1][i], String_data_enc[2][i], 
-                                String_data_enc[3][i], String_data_enc[4][i], String_data_enc[5][i],
-                                String_data_enc[6][i], String_data_enc[7][i], int_data_enc[0][i], 
-                                int_data_enc[2][i], int_data_enc[1][i], int_data_enc[3][i]);
-        }
+        nuevo_encargado("55555555E", "Cataluña", "Albert", "Rivera", 
+                        "Diaz", "05/05/1985", "05/05/2005", "fin de semana", 
+                        565676787, 662872195, 5, 505);
     }
 }
